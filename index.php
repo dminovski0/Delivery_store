10a1081c98f5bf1d4fdc21b7eef5aa194b9b7be2
DMinovski <daniel_minovski@hotmail.com>
2020-07-14 20:48:15 +0200

    Adding an index page

---
+++ index.php
@@ -0,0 +1,355 @@
+<?php
+
+<<<END
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap" rel="stylesheet" />
+        <!-- The two items are media responsive, they align under each other when the users use mobile devices 
+        	daniel_minovski@hotmail.com
+        -->
+        <title>
+            New pizza task
+        </title>
+    </head>
+    <style>
+        .box {
+            vertical-align: top;
+            box-sizing: border-box;
+            height: auto;
+        }
+        @media screen and (max-width: 768px) {
+            .box {
+                display: block;
+            }
+            .one {
+                width: 100%;
+            }
+            .two {
+                width: 100%;
+            }
+            .three {
+                width: 100%;
+            }
+            .four {
+                width: 100%;
+            }
+            
+            .five {
+                width: 100%;
+            }
+            .six {
+                width: 100%;
+            }
+        }
+    </style>
+    <body>
+        <table align="center">
+            <tr>
+                <td class="box one" style="text-align: center;">
+                    <img src="Pizza logo.png" style="width:20em; height: 15em;"/>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <table align="center">
+            <tr>
+                <td style="color: CornflowerBlue;"><a href="">HOME</a></td>
+                <td style="padding: 10px;">|</td>
+                <td style="color: CornflowerBlue;">CART</td>
+                <td style="padding: 10px;">|</td>
+                <td style="color: CornflowerBlue;">ORDER</td>
+            </tr>
+        </table>
+        <br />
+        <table style="margin: 0px auto;">
+            <tr>
+                <td><p style="font-size: 200%; font-weight: bold; text-align: center; padding: 0 30px;">Pizza Place</p></td>
+            </tr>
+        </table>
+        <br />
+        <table align="center">
+            <tr>
+                <td>
+                    <img src="Pizza types.jpg" style="max-width: 100%; width: 50em; height: 25em" />
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <table align="center">
+            <tr>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Cheese  <br />
+                        Pizza
+                    </p>
+                    <p><img src="Cheese Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify; height: 10em;">It should be no shocker that a classic is the statistical favorite. Cheese pizza is one of the most popular choices. It will always be a simple, unadorned masterpiece on its own.</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+                <td class="box two" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Veggie  <br />
+                        Pizza
+                    </p>
+                    <p><img src="Veggie Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify;  height: 10em;">When you want to jazz up your cheese pizza with color and texture, veggies are the perfect topping. And you’re only limited by your imagination. Everything from peppers and mushrooms, to eggplant and onions make for an exciting and tasty veggie pizza.</p>
+                    <p>$249</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Pepperoni  <br />
+                        Pizza
+                    </p>
+                    <p><img src="Pepperoni Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify;  height: 10em;">There’s a reason this is one of the most popular types of pizza. Who doesn’t love biting into a crispy, salty round of pepperoni?</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Meat  <br />
+                        Pizza
+                    </p>
+                    <p><img src="Meat Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify;  height: 10em;">If pepperoni just isn’t enough, and you’re looking for a pie with a bit more heft, a meat pizza is a perfect and popular choice. Pile on ground beef and sausage for a hearty meal.</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <br />
+                    <br />
+                    <br />
+                </td>
+            </tr>
+            
+            
+            <tr>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Margherita  <br />
+                        Pizza
+                    </p>
+                    <p><img src="Margherita Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify;  height: 10em;">Deceptively simple, the Margherita pizza is made with basil, fresh mozzarella, and tomatoes. There’s a reason it’s an Italian staple and one of the most popular types of pizza in the country.</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        BBQ Chicken  <br />
+                        Pizza
+                    </p>
+                    <p><img src="BBQ Chicken Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify;  height: 10em;">If you love BBQ chicken and you love pizza, why not put them together? This has long been a cult favorite of sports fans and college kids. The chicken slathered over the top of a pie gives it a tangy, sweet flavor that can’t be beaten.</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Hawaiian  <br />
+                        Pizza
+                    </p>
+                    <p><img src="Hawaiian Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify;  height: 10em;">Pineapple might not be the first thing that comes to mind when you think pizza. But add in some ham and it creates an unexpectedly solid sweet and salty combination for this type of pizza.</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Buffalo  <br />
+                        Pizza
+                    </p>
+                    <p><img src="Buffalo Pizza.jpg" style="width:7em; height: 7em;"/></p>
+                    <p style="font-size: 75%; font-weight: bold; text-align: justify;  height: 10em;">Who says your pizza has to be strictly tomato-sauce based? Branch out with some buffalo sauce on your pie. All its spicy, salty, buttery goodness is a natural pairing for pizza.</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ADD TO CART</button></p>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+
+        <table align="center">
+            <tr>
+                <td class="box three" style="font-size: 100%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    
+                    <p><img src="cc.png" /></p>
+                    
+                   
+                </td>
+                <td class="box four" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p style="color:gray;">
+                        Limited Time Only*<br />
+                    </p>
+                    <p style="font-size:200%; color:khaki;">$11.99!
+                        
+                    </p>
+                    <p style="text-decoration: line-through;">$249</p>
+                    
+                    
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 4em; width: 8em;">GET YOURS</button></p>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <table align="center">
+            <tr>
+                <td class="box five" style="font-size: 100%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        1 Royal Berkey System <br />
+                        1 Flouride Reduction Filter <br />
+                        1 Stainless Steel Spigot <br />
+                        1 Stainless Steel Stand <br />
+                        1 Water Carafe <br />
+                        1-6 Pack Boroux Basics<br />
+                        Bottles 
+                    </p>
+                </td>
+                <td class="box six" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    
+                    
+                    <p><button style="background-color: lightgreen; font-weight: bold; height: 5em; width: 10em;">ENTER TO WIN!</button></p>
+                    <p style="font-style:italic; font-size: 100%; font-weight: normal; color:black;">Like & Follow Us on</p>
+                    <p><img src="f.png" style="width: 30px; height: 30px;"/><img src="i.png" style="width: 30px; height: 30px;"/><img src="t.png" style="width: 30px; height: 30px;"/></p>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <table align="center" style="background-color:lightgrey; padding:10 0px">
+            <tr>
+                <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Boroux <br />
+                        1-Liter Bottle
+                    </p>
+                    <p><img src="cc.png" /></p>
+                    <p>Limited Quantity</p>
+                    <p>$29.99</p>
+                    <p><button style="background-color: lightgrey; font-weight: bold; height: 5em; width: 10em; border-width: :5px; border-color:CornflowerBlue;  border-radius: 25px">GET YOURS>></button></p>
+                </td>
+                <td class="box two" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Sport <br />
+                        Berkey
+                    </p>
+                    <p><img src="dd.png" /></p>
+                    <p><br/></p>
+                    <p>$249</p>
+                    <p><button style="background-color: lightgrey; font-weight: bold; height: 5em; width: 10em; border-width: :5px; border-color:CornflowerBlue;  border-radius: 25px">GET YOURS>></button></p>
+                </td>
+                 <td class="box one" style="font-size: 150%; font-weight: bold; text-align: center; padding: 0 30px;">
+                    <p>
+                        Berkey Flouride <br />
+                        Filters
+                    </p>
+                    <p><img src="cc.png" /></p>
+                    <p><br/></p>
+                    <p>$29.99</p>
+                    <p><button style="background-color:lightgrey; font-weight: bold; height: 5em; width: 10em; border-width: :5px; border-color:CornflowerBlue; border-radius: 25px">GET YOURS>></button></p>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <table align="center">
+            <tr>
+                <td>
+                    <p style="font-style: italic; font-family: 'Cedarville Cursive', cursive; text-align: center; font-size: 5em; color: CornflowerBlue;">Love your pizza!</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="text-align: center; font-size: 150%;">Join Our 115+ Followers on Social Media</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="text-align: center; font-size: 150%;">& Share Your Pizza Love! <span style="font-size: 100%; color: CornflowerBlue;">#Pizzalove</span></p>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <table align="center">
+        	<tr>
+        		<td>
+        			<p>
+        				<img src="f.png" style="width: 30px; height: 30px;"/>
+        				<img src="i.png" style="width: 30px; height: 30px;"/>
+        				<img src="t.png" style="width: 30px; height: 30px;"/>
+        			<img src="f.png" style="width: 30px; height: 30px;"/>
+        		<img src="f.png" style="width: 30px; height: 30px;"/>
+        	</p>
+        		</td>
+        	</tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <table align="center">
+            
+            <tr>
+                <td>
+                    <p style="text-align: center; font-size: 150%;">Looking to learn More? <span style="font-weight: bold">Call</span>, <span style="font-weight: bold">Text</span> or <span style="font-weight: bold">Email us</span>!</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="text-align: center; font-size: 150%;">Ready to Order? Visit <span style="font-weight: bold">PizzaPlace.com</span> or give us a call.</p>
+                </td>
+            </tr>
+            <br />
+        <br />
+        <br />
+        </table>
+        <table align="center">
+        	<tr>
+            	<p style="color:CornflowerBlue; text-align: center; font-size:300%;">1-800-350-4170</p>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <table align="center">
+            <tr>
+                <td style="color: CornflowerBlue;">Update Subscription</td>
+                <td style="padding: 10px;">|</td>
+                <td style="color: CornflowerBlue;">Unsubscribe</td>
+                <td style="padding: 10px;">|</td>
+                <td style="color: CornflowerBlue;">View Online</td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <table align="center">
+            <tr>
+                <td style="padding: 10px; text-align: center;">
+                	<p>Copyright &copy; Pizza Place</p>
+                	<p>1976 Aspen Circle Pueblo, Colorado 81006</p>
+                </td>
+            </tr>
+                
+        </table>
+    </body>
+</html>
+
+END;
+?>
